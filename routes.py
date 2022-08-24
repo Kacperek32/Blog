4afab37426d77c317e0c49703ce6dec5f6517d70
Kacper Manterys <kacpermanterys@gmail.com>
2022-08-25 00:19:09 +0200

    Add fix

--- routes.py
+++ routes.py
@@ -1,14 +1,20 @@
 from flask import render_template, request, redirect
 from blog import app
+import os
 from blog.forms import EntryForm
 from blog.models import Entry, db
+from flask import render_template, request, session, flash, redirect, url_for
+from blog.forms import LoginForm
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
 def homepage():
     posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
     return render_template('index.html', posts=posts)
 
-@app.route('/addpost', methods=["GET","POST"])
+@app.route('/new-post', methods=["GET","POST"])
 def create_entry():
     form = EntryForm()
     errors = None
@@ -38,3 +44,31 @@ def edit_entry(entry_id):
         else:
             errors = form.errors
     return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
+
+@app.route("/drafts/", methods=['GET'])
+def list_drafts():
+   drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+   return render_template("drafts.html", drafts=drafts)
