47c8765046b8c716f2fe47f19104a2935b32c3c6
Kacper Manterys <kacpermanterys@gmail.com>
2022-08-25 00:18:47 +0200

    Add fix

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
-
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
 
 class Config:
+    ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "telewizor55")
     SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"   
     SQLALCHEMY_DATABASE_URI = (                           
            os.environ.get('DATABASE_URL') or
