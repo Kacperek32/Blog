cf82ee88b654a27767f36a6345c378926a0d0514
Kacper Manterys <kacpermanterys@gmail.com>
2022-08-25 00:18:30 +0200

    Add fix

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -14,3 +16,7 @@ def make_shell_context():
     "db": db,
     "Entry": models.Entry
   }
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
